95bc88e0c5d5190603f2da3fb77e9d5c5347a371
Pro-Max-420 <121747358+Pro-Max-420@users.noreply.github.com>
2023-01-18 02:04:39 +0600

    Add files via upload

---
+++ Script-Gift.py
@@ -0,0 +1,8 @@
+import os,time
+print('\033[0;92m\n Congratulations! Your Device Support This Tools\033[1;37m')
+time.sleep(5)
+print('\033[0;93m\n JOIN MY SCRIPT GIFT GROUP\033[1;37m')
+import R4NDOM
+time.sleep(3)
+R4NDOM.main()
+ 
